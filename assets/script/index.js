75fe1f14037de49c7b27ba877525648663d63b21
Marco Preciado <mac46005@gmail.com>
2022-06-05 10:57:34 -0500

    creating simple script that applies styles to nav btns

---
+++ assets/script/index.js
@@ -0,0 +1,13 @@
+var elmainContent = document.querySelector('main-content');
+var elContentItems = document.querySelectorAll('content-item');
+var elNavBtn = document.querySelectorAll('nav-btn');
+
+function navSelected(){
+    for (let i = 0; i < elNavBtn.length; i++) {
+        elNavBtn[i].addEventListener('clicked', function(){
+            
+        });
+    }
+}
+
+

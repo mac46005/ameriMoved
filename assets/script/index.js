5dfa53cb28048cc76ad3b3faa49b85e77bb4201d
Marco Preciado <mac46005@gmail.com>
2022-06-05 12:27:30 -0500

    need to practice javascript

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,13 +1,17 @@
-var elmainContent = document.querySelector('main-content');
-var elContentItems = document.querySelectorAll('content-item');
-var elNavBtn = document.querySelectorAll('nav-btn');
+var elmainContent = document.querySelector(".main-content");
+var elContentItems = document.querySelectorAll(".content-item");
+var elNavBtn = document.querySelectorAll(".nav-btn");
 
-function navSelected(){
-    for (let i = 0; i < elNavBtn.length; i++) {
-        elNavBtn[i].addEventListener('clicked', function(){
-            
-        });
-    }
+function navSelected() {
+  for (let i = 0; i < elNavBtn.length; i++) {
+    elNavBtn[i].addEventListener('click',function() {
+      let currentBtn = document.querySelectorAll(".selected");
+    //   currentBtn[0].classList.replace('selected','');
+currentBtn[0].classList.replace('selected','');
+
+      this.className += " selected";
+    });
+  }
 }
 
-
+navSelected();

99829f7afc6d4df946168daa388f05aa79b21a66
Marco Preciado <mac46005@gmail.com>
2022-06-06 18:53:11 -0500

    update script

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,6 +1,7 @@
-var Content = document.querySelector(".main-content");
+var mainContent = document.querySelector(".main-content");
 var contentItems = document.querySelectorAll(".content-item");
 var navBtns = document.querySelectorAll(".nav-btn");
+var mainBody = document.querySelector('.main-body');
 
 
 // Think about a way to transition the element by applying opacity
@@ -9,15 +10,22 @@ function pageTransition(){
         navBtns[i].addEventListener('click',function(){
             // changes current btn to not selected
             // and changes the newly clicked button to selected
-            
+
             var currentBtn = document.querySelectorAll('.selected');
             var arr = ['selected'];
             currentBtn[0].classList.remove(arr);
             navBtns[i].className += ' selected';
 
-
         });
     }
+
+    mainBody.addEventListener('click', (e) => {
+        console.log(e.target);
+        // var id = e.target.dataset.id;
+        // console.log(id);
+        console.log(e.target.dataset.id);
+    });
+
 
 
 }

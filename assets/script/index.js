d7cec2b6de4a3b82045a83d1f48bf1d145989dd2
Marco Preciado <mac46005@gmail.com>
2022-06-06 10:20:01 -0500

    continue transition script

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,8 +7,20 @@ var navBtns = document.querySelectorAll(".nav-btn");
 function pageTransition(){
     for (let i = 0; i < navBtns.length; i++) {
         navBtns[i].addEventListener('click',function(){
+            var currentBtn = document.querySelectorAll('.selected');
+            var arr = ['selected'];
+            currentBtn[0].classList.remove(arr);
             navBtns[i].className += ' selected';
-        });        
+        });
+        ConstantSourceNode.addEventListener('click',(e) => {
+            const id = e.target.dataset.id;
+
+            console.log(id);
+
+            if(id){
+                // change views
+            }
+        })
     }
 }
 pageTransition();
